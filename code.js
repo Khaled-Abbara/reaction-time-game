9cc211e64203fbcfef69996942a0c98f41ea036a
Khaled Abbara <148062226+Khaled-Abbara@users.noreply.github.com>
2026-04-12 17:38:18 +0800

    Submit

--- code.js
+++ code.js
@@ -4,16 +4,27 @@ const gamePage = document.getElementById("game-page")
 const gameContainer = document.getElementById("game-container")
 const startGameBtn = document.getElementById("start-game-btn")
 
+const COLORS = {
+    active: "red",
+    success: "green",
+    default: "white"
+};
 
-const gameStats = {
+const gameState = {
     timer: 0,
-    score: 0
+    score: 0,
+    boxes: []
 }
 
 startGameBtn.addEventListener("click", ()=> {
-     createBoxes(9)
+    startGame()
 })
 
+function startGame() {
+    menuPage.style.display = "none"
+    createBoxes(9)
+    highlightRandomBox()
+}
 
 function createBoxes(maxBoxCount) {
     for (let c = 0; c < maxBoxCount; c++) {
@@ -21,5 +32,16 @@ function createBoxes(maxBoxCount) {
         box.id = c;
     
         gameContainer.appendChild(box);
+        gameState.boxes.push(box);
+
     }
+}
+
+function highlightRandomBox() {
+    const index = Math.floor(Math.random() * gameState.boxes.length);
+    const box = gameState.boxes[index];
+
+     box.style.backgroundColor = COLORS.active;
+
+
 }
