2f94ee6e729ab472a64ac8df03db2984232f2be6
Khaled Abbara <148062226+Khaled-Abbara@users.noreply.github.com>
2026-04-20 14:36:03 +0800

    submit

---
+++ db.js
@@ -0,0 +1,50 @@
+import {
+  firebaseConfig,
+  initializeApp,
+  getDatabase,
+  ref,
+  get,
+  set,
+  push,
+  update,
+  onValue,
+} from "./firebase.js";
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+async function getSpecificUser(userId) {
+  const snapshot = await get(ref(db, "users/" + userId));
+
+  if (!snapshot.exists()) {
+    return { data: null, error: "User not found" };
+  }
+  return { data: snapshot.val(), error: null };
+}
+
+async function getUsers() {
+  const snapshot = await get(ref(db, "users"));
+
+  if (!snapshot.exists()) {
+    return { data: null, error: "No users found" };
+  }
+
+  return { data: snapshot.val(), error: null };
+}
+
+async function setNewUser(username, password) {
+  const newUserRef = push(ref(db, "users"));
+  try {
+    set(newUserRef, {
+      username,
+      password,
+      score: 0,
+    });
+    return { success: true, data: newUserRef.key };
+  } catch (error) {
+    return { success: false, data: null };
+  }
+}
+
+export { getSpecificUser, getUsers };
+export { setNewUser };
